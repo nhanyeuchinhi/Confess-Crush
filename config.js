40b26c7e3fd11c9c8b7305f0da3c39e5bbbf6a7a
nhanyeuchinhi <106918287+nhanyeuchinhi@users.noreply.github.com>
2022-06-05 21:18:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Website Được Lập Ra Và Phát Triển Bởi Lê Ngọc Nhân",
+    introTitle: 'Ng Hoàng Gia Nhi - Hai :3',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -12,9 +12,9 @@ const CONFIG = {
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <3',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messDesc: 'Tối nay cỡ mấy giờ á, call tí nha :3',
+    btnAccept: 'okeee <3',
+    messLink: 'https://www.facebook.com/nhan135.vpbq/' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
